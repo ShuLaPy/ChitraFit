2748848c4d44dee92bb86b2a7a220cf6fe41949c
Shubham Lad <shubhamlad1001@gmail.com>
2022-01-16 11:01:29 +0530

    Error handling

--- src/server.js
+++ src/server.js
@@ -5,6 +5,7 @@ const helmet = require("helmet");
 const compression = require("compression");
 const {sequelize} = require("./sequelize");
 const routes = require('./routes/v1');
+const { errorConverter, errorHandler } = require("./middlewares/error");
 
 const app = express();
 
@@ -20,6 +21,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(compression());
 
 app.use('/api/v1', routes);
+
+// convert error to ApiError, if needed
+app.use(errorConverter);
+
+// handle error
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5500;
 
